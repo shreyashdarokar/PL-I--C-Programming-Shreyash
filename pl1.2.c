cf352bd98a6e51b39516091ed0d6fbe8f93959fa
Shreyash Darokar <darokarshreyash0717@gmail.com>
2026-09-03 00:22:21 +0530

    Add files via upload

---
+++ pl1.2.c
@@ -0,0 +1,17 @@
+/*
+Program 2 -----> Write a program to calculate the area of a circle
+Solution -----> User defined values
+*/
+
+#include<stdio.h>
+void int()
+
+{ 
+	float radius, area;
+	printf("\n Enter the radius of the circle: ");
+	scanf("%f", &radius);
+	area = (3.14*radius*radius);
+	printf("\n Area of Circle = %f", area);
+	
+	return 0;
+}
